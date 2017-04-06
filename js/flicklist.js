8e221ea088f987f41dea0a3639ebc8874c61f8fa
Jaime Gilligan <gilligan.jme@gmail.com>
2017-04-06 17:18:52 -0500

    studio 3

--- js/flicklist.js
+++ js/flicklist.js
@@ -14,7 +14,7 @@ var model = {
 
 var api = {
   root: "https://api.themoviedb.org/3",
-  token: "8e888fa39ec243e662e1fb738c42ae99" // TODO 0 add your api key
+  token: "6e0b00dca26af70629b4d7f718f433cb" // TODO 0 add your api key
 }
 
 
@@ -39,7 +39,7 @@ function discoverMovies(callback) {
 
 
 /**
- * Makes an AJAX request to the /search/movie endpoint of the API, using the 
+ * Makes an AJAX request to the /search/movie endpoint of the API, using the
  * query string that was passed in
  *
  * if successful, updates model.browseItems appropriately and then invokes
@@ -72,7 +72,7 @@ function render() {
       .append(title)
       // TODO 3
       // give itemView a class attribute of "item-watchlist"
-
+      
     $("#section-watchlist ul").append(itemView);
   });
 
@@ -89,26 +89,25 @@ function render() {
       // the button should be disabled if this movie is already in
       // the user's watchlist
       // see jQuery .prop() and Array.indexOf()
-
+      button.prop("disabled", model.watchlistItems.indexOf(movie) !== -1);
 
     // TODO 1
     // create a paragraph containing the movie object's .overview value
     // then, in the code block below,
     // append the paragraph in between the title and the button
+    var paragraph = $("<p></p>").text(movie.overview);
+
 
 
     // append everything to itemView, along with an <hr/>
     var itemView = $("<li></li>")
       .append($("<hr/>"))
       .append(title)
+      .append(paragraph)
       .append(button);
 
     // append the itemView to the list
     $("#section-browse ul").append(itemView);
   });
-  
+
 }
-
-
-
-
